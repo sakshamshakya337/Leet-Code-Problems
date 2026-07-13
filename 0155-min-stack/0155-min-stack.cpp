fe8e98a42d8091b17a9239a0284bbb0b3a27a1dd
Saksham shakya <80311222+sakshamshakya337@users.noreply.github.com>
2026-07-13 16:44:36 +0530

    Time: 60 ms (38.72%), Space: 151 MB (68.04%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,14 +1,27 @@
 class MinStack {
-    stack<int> s, st;
 public:
+    stack<pair<int, int>> st;
+    MinStack() {
+        
+    }
+    
     void push(int val) {
-        s.push(val);
-        if (st.empty() || val <= st.top()) st.push(val);
+        if (!st.empty()) {
+            st.push({val, min(val, st.top().second)});
+        } else {
+            st.push({val, val});
+        }
     }
+    
     void pop() {
-        if (s.top() == st.top()) st.pop();
-        s.pop();
+        st.pop();
     }
-    int top() { return s.empty() ? -1 : s.top(); }
-    int getMin() { return st.empty() ? -1 : st.top(); }
+    
+    int top() {
+        return st.top().first;
+    }
+    
+    int getMin() {
+        return st.top().second;
+    }
 };
